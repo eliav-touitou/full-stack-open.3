83d2bfd3ed0b16ce52287c8a9abc354790552f55
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-03-08 00:08:57 +0200

    gitignore

---
+++ build/precache-manifest.4446b18e4907bde416726cc17533b7d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "605b4d2fd68151445df0135828319d29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf402600554bd22b0e9e",
+    "url": "/static/css/main.4187a5f6.chunk.css"
+  },
+  {
+    "revision": "1224e9f809dff3239121",
+    "url": "/static/js/2.870ca22c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.870ca22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf402600554bd22b0e9e",
+    "url": "/static/js/main.dd94fa96.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
